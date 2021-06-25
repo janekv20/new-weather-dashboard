9eccd0f684af9c71b7e04828543650b26ef3c495
Pint <83248746+janekv20@users.noreply.github.com>
2021-06-25 09:57:23 -0500

    created initial html page and weather.js file

---
+++ assets/js/weather.js
@@ -0,0 +1,12 @@
+var cityFormEl = document.querySelector("#city-form");
+var cityInputEl = document.querySelector("#city");
+
+var getWeatherRepo = function (weather) {
+    //format the weather api url
+    var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+
+
+    fetch("");
+};
+
+getWeatherRepo();
