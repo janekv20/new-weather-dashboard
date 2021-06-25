ac681fb17ace44f56fa7aa3ac68c754a30f370a5
Pint <83248746+janekv20@users.noreply.github.com>
2021-06-25 10:13:10 -0500

    added api key and search form

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,12 +1,30 @@
 var cityFormEl = document.querySelector("#city-form");
 var cityInputEl = document.querySelector("#city");
+var apiKey = "38de1bdb444f9c9452356228729d5cbc";
+
+var formSubmitHandler = function (event) {
+    event.preventDefault();
+    var city = cityInputEl.value.trim();
+
+    if (city) {
+        getWeatherRepo(city);
+        cityInputEl.value = "";
+    } else {
+        alert("Enter a City");
+    }
+    console.log(event);
+}
 
 var getWeatherRepo = function (weather) {
     //format the weather api url
     var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
 
-
-    fetch("");
+    //make a request to the url
+    fetch(apiUrl).then(function (response) {
+        response.json().then(function (data) {
+            console.log(data);
+        });
+    });
 };
 
-getWeatherRepo();
+cityFormEl.addEventListener("submit", formSubmitHandler);
